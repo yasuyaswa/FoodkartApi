f22f32389bf16169e747b75096f9a20e42394f94
yasuyaswa <83386305+yasuyaswa@users.noreply.github.com>
2025-08-31 11:17:04 +0530

    Update AdminsController.cs

--- FoodkartApi/Controllers/AdminsController.cs
+++ FoodkartApi/Controllers/AdminsController.cs
@@ -14,7 +14,7 @@ namespace FoodkartApi.Controllers
     [Route("api/[controller]")]
     [ApiController]
     //for sample comment added for testing purposes
-    //1243546
+    //1243546 789
     public class AdminsController : ControllerBase
     {
         private readonly FoodAppContext _context;
